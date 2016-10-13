d54fd715f16846fdc37a1841cd1256aa1101ee51
juancholkovich <judhenaosa@unal.edu.co>
2016-10-13 13:55:17 -0500

    documents

--- R/get.info.R
+++ R/get.info.R
@@ -1,6 +1,18 @@
-# get.info
-# Juan David Henao Sanchez
+#' @export get.info
+#' @author Juan David Henao <judhenaosa@unal.edu.co>
+
 # Bioinformatics and Systems Biology | Universidad Nacional de Colombia
+
+#' @title Download raw expression data from GEO DataSet
+#' @description Using a GSE ID list, from the GEO DtaSet ftp, search and download each one of the raw expression data in a 
+#' separate file with the GSE ID as the name, after that, this function uncompress the downloaded file to be ready to use.
+#' Adittionally, this function search and downloaded the information of the chip using the GPL ID to obtain the .soft file.
+#' @usage get.info(GSE, GPL, dir=".")
+#' @param GSE The list of GSE ID.
+#' @param GPL The GPL ID of the chip used in the microarray experiment.
+#' @param dir The pathfile to download and uncompress the raw wxpewssion data, by default is the actual pathfile.
+#' @return A set of files with the uncompressed data and ready to use.
+#' @seealso \code{\link{get.affy}} to charge the expression data.
 
 get.info <- function(GSE,GPL,dir="."){
   
