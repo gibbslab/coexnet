3c71c8ddb5dcc7049ecd67d17531f4328ac5b89a
juancholkovich <judhenaosa@unal.edu.co>
2016-10-19 14:45:26 -0500

    documents

--- R/cof.var.R
+++ R/cof.var.R
@@ -4,13 +4,11 @@
 # Bioinformatics and Systems Biology | Universidad Nacional de Colombia
 
 #' @title Calculate the mean and coefficient of variation of the expression matrix to cases, controls or all the samples
-#' @description Using a expression matrix, calculates the mean and the coefficient of variation to the probeset or genes using
-#' only the cases samples or control samples or using all the samples.
+#' @description Using a expression matrix, calculates the mean and the coefficient of variation to the probeset or genes using only the cases samples or control samples or using all the samples.
 #' @param data A data frame with the expression matrix.
 #' @param type The samples to be used, can be "control", "case" or "complete".
 #' @param treatment A vector of 0 and 1 values, 0 are the control samples and 1 are the cases samples.
-#' @return A data frame with the expression matrix and two new colums. one to the mean and the another one to the 
-#' coefficient of variation values.
+#' @return A data frame with the expression matrix and two new colums. one to the mean and the another one to the coefficient of variation values.
 
 cof.var <- function(data,type,treatment){
   
