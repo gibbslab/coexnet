10f568e8fc7aa588f2a807e289edb8d524ee72fd
juan henao <judhenaosa@unal.edu.co>
2016-11-07 21:38:14 -0500

    vsn

--- R/expr.mat.R
+++ R/expr.mat.R
@@ -31,7 +31,7 @@ expr.mat <- function(affy,genes,NormalizeMethod,SummaryMethod){
     
     pvsn <- normalize.AffyBatch.vsn(affy)
    
-    vsn <- computeExprSet(x = pvsn,pmcorrect.method = "pmonly",summary.method = "avgdiff")
+    vsn <- computeExprSet(x = pvsn,pmcorrect.method = "pmonly",summary.method = "medianpolish")
     
     cat("Summarizing",sep = "\n")
     
