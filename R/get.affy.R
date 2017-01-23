fdd4a077d83e49c432eef00c26049a09d71a1c22
juancholkovich <judhenaosa@unal.edu.co>
2017-01-23 14:30:47 -0500

    Update coexnet
    Former-commit-id: 17f289e83f4531c5443001b91e0686713fbf4c78
    Former-commit-id: 5a34726b095a4c039ecabe883a3e45a0b8c7b474

--- R/get.affy.R
+++ R/get.affy.R
@@ -4,17 +4,23 @@
 #' @description Charges the data from a file with the GSM samples compressed using the "filelist.txt" file to identify the GSM ID
 #' and create the AffyBatch object finally.
 #' @param GSE The name of the file with the compressed samples data.
-#' @seealso \code{\link{get.info}} to download expression data from GEO DataSet.
+#' @param dir The pathfile where the samples and the filelist file are to create the AffyBatch object, by default is the current directory.
+#' @seealso \code{\link{get.info}} to download expression data from GEO DataSet included the filelist file.
 #' @examples 
 #' 
 #' # Load the AffyBatch from downloaded raw data
+#' # The data is from partial experiment results from Greene JG (GSE4773)
 #' 
-#' affy <- get.affy("GSE8216")
+#' affy <- get.affy(GSE = "GSE1234",dir = system.file("extdata",package = "coexnet"))
 
 
 # Bioinformatics and Systems Biology | Universidad Nacional de Colombia
 
-get.affy <- function(GSE){
+get.affy <- function(GSE,dir="."){
+  
+  # Change the directory 
+  
+  setwd(dir)
   
   # Reads the filelist.txt file with the samples information
   
