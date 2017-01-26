983ba1fc73703704f8d3a74450202c50560660ca
juancholkovich <judhenaosa@unal.edu.co>
2017-01-26 14:43:39 -0500

    Update coexnet
    Former-commit-id: 9ea012b10f80535f94aecdf0fed147ab28dac660
    Former-commit-id: 204b5ed2cbf4f8bc143246391b810a141adc5c7e

--- inst/unitTests/test_coexnet.R
+++ inst/unitTests/test_coexnet.R
@@ -2,29 +2,29 @@
 
 test_cof.var <- function(){
   
-  ## correct case
+  ## Correct cases
   
-  t <- c(rep(0,10),rep(1,10))
+  t <- c(0,1,0,1)
   
-  mat <- as.matrix(rexp(200, rate = 1))
-  norm <- t(apply(mat, 1, function(nm) rnorm(20, mean=nm, sd=1)))
+  a <- seq(0.1,0.5,length.out = 20)
+  norm <- data.frame(a,a+0.2,a+0.1,a+0.3)
   
   # complete
   
   result <- cof.var(norm)
-  checkEqualsNumeric(round(result[1,1],6),2.675794)
+  checkEqualsNumeric(round(result$cv[1],7),0.5163978)
   
   # case
   
   result2 <- cof.var(norm,complete = FALSE,treatment = t,type = "case")
-  checkEqualsNumeric(round(result2[1,1],6),2.137933)
+  checkEqualsNumeric(round(result2$cv[1],7),0.2020305)
   
   # control
   
   result3 <- cof.var(norm,complete = FALSE,treatment = t,type = "control")
-  checkEqualsNumeric(round(result3[1,1],6),2.675794)
+  checkEqualsNumeric(round(result3$cv[1],7),0.4714045)
   
-  ## error
+  ## Errors
   
   checkException(cof.var(),silent = TRUE)
   checkException(cof.var(norm[1,3]),silent = TRUE)
