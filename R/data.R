b55e5f64d1bbbc861fe8c170df740047d68eb99c
juancholkovich <judhenaosa@unal.edu.co>
2017-01-26 12:20:41 -0500

    Update coexnet
    Former-commit-id: 2806121a34c7ca1895edc92b3cbf94bff32225a8
    Former-commit-id: 06bedefc224d59bbae7121439bc5c51ec2a06476

---
+++ R/data.R
@@ -0,0 +1,7 @@
+## affy documentation
+
+#' @name affy.
+#' @docType data.
+#' @description AffyBatch object, has a part of raw expression value obtained from microarray chip.
+#' @source GEO Dataset from NCBI, GEO accession: GSE4773.
+#' @author Greene JG, 2006.
