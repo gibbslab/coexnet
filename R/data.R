95c271e098212cf11c317271c032762ddc1793d3
juancholkovich <judhenaosa@unal.edu.co>
2017-01-26 12:38:43 -0500

    Update coexnet
    Former-commit-id: bc1b3588642cfe82c5b3c2bb9d8b56056034aa2a
    Former-commit-id: 0848d91d23cd4d005a5b5d4a25e06d743613fc94

--- R/data.R
+++ R/data.R
@@ -3,7 +3,15 @@
 #' @title Greene-5P01NS017771-220003: Microarray expression experiment related with Parkinson disease.
 #' @name affy
 #' @docType data
-#' @description AffyBatch object, has a part of raw expression value obtained from microarray chip.
+#' @description AffyBatch object, has a part of raw expression values obtained from microarray chip.
 #' @source GEO Dataset from NCBI, GEO accession: GSE4773.
+#' @author Greene JG, 2006.
+NULL
+
+#' @title Table with the relation between the probesets and the corresponding gene.
+#' @name info
+#' @docType data
+#' @description Table where the probesets and genes are related.
+#' @source GEO Dataset from NCBI, GPL accession: GPL570.
 #' @author Greene JG, 2006.
 NULL
