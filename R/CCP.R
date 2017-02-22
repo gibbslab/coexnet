2a2045e3139d748266ac82fc29151338babc1d61
Juan Henao <judhenaosa@unal.edu.co>
2017-02-22 11:18:59 -0500

    new function
    Former-commit-id: a7490fde64e3f833d096a01f8c6ee0deae036c96
    Former-commit-id: b5c9991f294293dbdd4e76e1ef976fc23ee80b31

--- R/CCP.R
+++ R/CCP.R
@@ -1,4 +1,19 @@
-lapply(seq_along(b$csize)[b$csize > 1], function(x) 
-  V(a)$name[b$membership %in% x])
+neta <- read.graph(file = "Esclerosis.net",format = "ncol")
+netb <- read.graph(file = "Parkinson.net",format = "ncol")
+
+intersect <- graph.intersection(neta,netb,keep.all.vertices = FALSE)
+
+
+#### Funcion
+
+members <- which(graph.coreness(intersect) > 0)
+
+CCP <- remove.edge.attribute(graph = intersect,name = members)
+
+lapply(seq_along(graph$csize)[graph$csize > 1], function(x) 
+  V(intersect)$name[graph$membership %in% x])
+
 
 table(names(V(ESC)) %in% names(V(PRK)))
+
+V(intersect)
