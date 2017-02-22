2f70805be9650fbdc8d69b1af943e131e1bb346c
Juan Henao <judhenaosa@unal.edu.co>
2017-02-22 11:40:39 -0500

    new function
    Former-commit-id: c67fa82b093d85455fc8562518f1843ee678f240
    Former-commit-id: d7e923d8764b6dd50a242cc2a795e836b948a5a1

--- R/CCP.R
+++ R/CCP.R
@@ -6,14 +6,6 @@ intersect <- graph.intersection(neta,netb,keep.all.vertices = FALSE)
 
 #### Funcion
 
-members <- which(graph.coreness(intersect) > 0)
+members <- which(degree(intersect) == 0)
 
-CCP <- remove.edge.attribute(graph = intersect,name = members)
-
-lapply(seq_along(graph$csize)[graph$csize > 1], function(x) 
-  V(intersect)$name[graph$membership %in% x])
-
-
-table(names(V(ESC)) %in% names(V(PRK)))
-
-V(intersect)
+remove.vertex.attribute(graph = intersect,name = names(members)[1])
