955aca5b749539b36b1b049f53fec9f2413febdc
Juan Henao <judhenaosa@unal.edu.co>
2017-02-22 18:36:00 -0500

    new function
    Former-commit-id: 770d8e62c82f57abaedb34bc45be08c9fe7309d3
    Former-commit-id: 13ae9e57ecafc45ea453d773b093e4f2255b58d0

--- R/ppi.net.R
+++ R/ppi.net.R
@@ -2,9 +2,12 @@ PRK <- read.table(file = "GSE9807_0,3.txt")
 genes <- as.data.frame(row.names(PRK))
 names(genes) <- "gene"
 
+
+### Function
+
 database <- STRINGdb$new(version="10",species=9606,score_threshold=0,input_directory="")
 mapped <- database$map(genes,"gene",removeUnmappedRows = TRUE)
 
-proteins <- mapped$STRING_id
+interactions <- database$get_interactions(mapped$STRING_id)
 
-interactions <- database$get_interactions(names(genes))
+
