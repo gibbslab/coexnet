68174c9c2aae47915f4c5f7b96ac723b3c8ba3c9
Juan Henao <judhenaosa@unal.edu.co>
2017-02-23 15:25:03 -0500

    new function
    Former-commit-id: 753af4a533a84f27f2475653e7b064edce59457c
    Former-commit-id: c22cc48ce320ca6f2ef28c76d6f38c21a9e20dfd

--- R/ppi.net.R
+++ R/ppi.net.R
@@ -1,57 +1,60 @@
-PRK <- read.table(file = "GSE9807_0,3.txt",stringsAsFactors = FALSE)
-genes <- as.vector(row.names(PRK))
-
-### Function
-
-for(n in genes){
-  if(grepl("-",n) == TRUE){
-    temp <- strsplit(n,"-")
-    genes <- genes[genes != n]
-    sepstr <- rapply(temp,c)
-    genes <- c(genes,sepstr)
-  }
-}
-
-new_genes <- as.data.frame(genes,stringsAsFactors = FALSE)
-names(new_genes) <- "gene"
-
-database <- STRINGdb$new(version="10",species=9606,score_threshold=0,input_directory="")
-mapped <- database$map(new_genes,"gene",removeUnmappedRows = TRUE)
-
-interactions <- database$get_interactions(mapped$STRING_id)
-
-evidence <- c("neighborhood","coexpression","experiments")
-graph_relations <- data.frame(interactions$from,interactions$to,stringsAsFactors = FALSE)
-
-for(i in evidence){
-  for(j in names(interactions)){
-    if(i == j){
-      graph_relations <- cbind(graph_relations,interactions[j])
+ppi.net <- function(genes,species_ID = 9606,evidence = c("neighborhood","neighborhood_transferred",
+            "fusion","cooccurence","homology","coexpression","coexpression_transferred",
+            "experiments","experiments_transferred","database","database_transferred","textmining",
+            "textmining_transferred","combined_score")){
+  
+  for(n in genes){
+    if(grepl("-",n) == TRUE){
+      temp <- strsplit(n,"-")
+      genes <- genes[genes != n]
+      sepstr <- rapply(temp,c)
+      genes <- c(genes,sepstr)
     }
   }
-}
-
-graph_ppi <- data.frame()
-
-for(i in 1:nrow(graph_relations)){
-  if(any(graph_relations[i,3:ncol(graph_relations)] > 0)){
-    graph_ppi <- rbind.data.frame(graph_ppi,graph_relations[i,],
-                                  stringsAsFactors = FALSE)
+  
+  new_genes <- as.data.frame(genes,stringsAsFactors = FALSE)
+  names(new_genes) <- "gene"
+  
+  database <- STRINGdb$new(version="10",species=species_ID,score_threshold=0,input_directory="")
+  mapped <- database$map(new_genes,"gene",removeUnmappedRows = TRUE)
+  
+  interactions <- database$get_interactions(mapped$STRING_id)
+  
+  graph_relations <- data.frame(interactions$from,interactions$to,stringsAsFactors = FALSE)
+  
+  for(i in evidence){
+    for(j in names(interactions)){
+      if(i == j){
+        graph_relations <- cbind(graph_relations,interactions[j])
+      }
+    }
   }
+  
+  graph_ppi <- data.frame()
+  
+  for(i in 1:nrow(graph_relations)){
+    if(any(graph_relations[i,3:ncol(graph_relations)] > 0)){
+      graph_ppi <- rbind.data.frame(graph_ppi,graph_relations[i,],
+                                    stringsAsFactors = FALSE)
+    }
+  }
+  
+  for(n in 1:nrow(graph_ppi)){
+    graph_ppi$interactions.from[n] <- mapped[
+      graph_ppi$interactions.from[n] == mapped$STRING_id,][1]
+  }
+  
+  for(n in 1:nrow(graph_ppi)){
+    graph_ppi$interactions.to[n] <- mapped[
+      graph_ppi$interactions.to[n] == mapped$STRING_id,][1]
+  }
+  
+  edge_list <- matrix()
+  edge_list <- cbind(rapply(graph_ppi$interactions.from,c))
+  edge_list <- cbind(edge_list,rapply(graph_ppi$interactions.to,c))
+  
+  final_graph <- graph.edgelist(edge_list,directed = FALSE)
+  
+  return(final_graph)
 }
 
-for(n in 1:nrow(graph_ppi)){
-  graph_ppi$interactions.from[n] <- mapped[
-    graph_ppi$interactions.from[n] == mapped$STRING_id,][1]
-}
-
-for(n in 1:nrow(graph_ppi)){
-  graph_ppi$interactions.to[n] <- mapped[
-    graph_ppi$interactions.to[n] == mapped$STRING_id,][1]
-}
-
-edge_list <- matrix()
-edge_list <- cbind(rapply(graph_ppi$interactions.from,c))
-edge_list <- cbind(edge_list,rapply(graph_ppi$interactions.to,c))
-
-final_graph <- graph.edgelist(edge_list,directed = FALSE)
