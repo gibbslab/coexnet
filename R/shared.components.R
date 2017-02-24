6076a72000ef2664e17f6e331b9a646bdad81f09
Juan Henao <judhenaosa@unal.edu.co>
2017-02-24 16:31:11 -0500

    Upgrade coexnet
    Former-commit-id: b50697495b5f066eadef2c1471770138dea58086
    Former-commit-id: b04bf5475e551026829d1bab2ad6e4501a1a8982

--- R/shared.components.R
+++ R/shared.components.R
@@ -1,9 +1,9 @@
 
 shared.components <- function(...){
-  
+  # Obtain the intersection set of the networks
   intersect <- graph.intersection(...,keep.all.vertices = FALSE)
-  
+  # Select the nodes in the intersected network with degree equal cero
   members <- which(degree(intersect) == 0)
-  
+  # Return the names of the alone nodes in the intersected network
   return(names(members))
 }
