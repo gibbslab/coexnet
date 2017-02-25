c7d2494f2f5d9a0f4f73abb7462249f9bbdcce10
Juan Henao <judhenaosa@unal.edu.co>
2017-02-24 23:06:55 -0500

    Upgrade coexnet
    Former-commit-id: 2d7f8fbe7f19fd39a0a858a5fdd5d20b0df433f6
    Former-commit-id: 971e90a25b77f5145d0f00eed3e0e73351613e0f

--- R/CCP.R
+++ R/CCP.R
@@ -1,3 +1,14 @@
+#' @export CCP
+#' @author Juan David Henao <judhenaosa@unal.edu.co>
+
+# Bioinformatics and Systems Biology | Universidad Nacional de Colombia
+
+#' @title Finding Common Connection Pattern between different networks
+#' @description From the ntersection of two or more networks, obntain the connected components deleting
+#' the alone nodes in the intersection network. The networks must be igraph objects
+#' @param ... The networks (igraph objects) to obtain the Common Connection Patterns
+#' @return An igraph object with all the Common Connection Pattern in the same network
+
 CCP <- function(...){
   # Obtain the intersection set of the networks
   intersect <- graph.intersection(...,keep.all.vertices = FALSE)
