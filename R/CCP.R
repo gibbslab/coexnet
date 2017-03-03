cd910bc9745a664db7216e5ad106e8204ae146e9
Juan Henao <judhenaosa@unal.edu.co>
2017-03-02 22:09:16 -0500

    Update coexnet
    Former-commit-id: 60cd74cc1f87949561d034f1716283026d9fd77c
    Former-commit-id: 12b66cb9a4f3b17a18ea425f59ffe7d0fd6ca34f

--- R/CCP.R
+++ R/CCP.R
@@ -4,8 +4,8 @@
 # Bioinformatics and Systems Biology | Universidad Nacional de Colombia
 
 #' @title Finding Common Connection Pattern between different networks.
-#' @description From the ntersection of two or more networks, obntain the connected components deleting
-#' the alone nodes in the intersection network. The networks must be igraph objects.
+#' @description From the intersection of two or more networks, it obtains the connected components 
+#' by deleting the solitary nodes in the intersection network. They must be graph objects.
 #' @param ... The networks (igraph objects) to obtain the Common Connection Patterns.
 #' @return An igraph object with all the Common Connection Pattern in the same network.
 #' @examples 
@@ -14,18 +14,18 @@
 #' data("net1")
 #' data("net2")
 #' 
-#' # Obtain Common Connection Patterns
+#' # Obtaining Common Connection Patterns
 #' 
 #' ccp <- CCP(net1,net2)
 #' ccp
 
 CCP <- function(...){
-  # Obtain the intersection set of the networks
+  # Obtains the intersection set of the networks
   intersect <- graph.intersection(...,keep.all.vertices = FALSE)
-  # Select the intersected nodes without any value of dregree
+  # Selects the intersection nodes without any value of degree
   members <- which(degree(intersect) == 0)
-  # Delete the aone vertices in the intersection network
+  # Deletes the solitary vertices in the intersection network
   CCP <- delete.vertices(graph = intersect,v = names(members))
-  # Return only the nodes in the intersection network with at least one edge
+  # Returns only the nodes in the intersection network with at least one edge
   return(CCP)
 }
