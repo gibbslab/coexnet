ec22dc89e94dea9395f30f256780d12968676f26
Juan Henao <judhenaosa@unal.edu.co>
2017-03-05 16:45:40 -0500

    Update coexnet
    Former-commit-id: d7df433deb050fb5cdb755046e4cea14a19d2a19
    Former-commit-id: 15de0ff3f47ff823ddd2640d0a94181bc0579400

--- R/get.info.R
+++ R/get.info.R
@@ -4,18 +4,15 @@
 # Bioinformatics and Systems Biology | Universidad Nacional de Colombia
 
 #' @title Download raw expression data from GEO DataSet
-#' @description Using a GSE ID list, from the GEO DtaSet ftp, search and download each one of the raw expression data in a 
-#' separate file with the GSE ID as the name, after that, this function uncompress the downloaded file to be ready to use.
-#' Adittionally, this function search and downloaded the information of the chip using the GPL ID to obtain the .soft file.
-#' @usage get.info(GSE, GPL, dir = ".")
+#' @description Using a GSE ID list, from the GEO DataSets FTP, it searches and downloads each raw expression data in a separate file with the GSE ID as the name. After, the function uncompresses the downloaded file to be ready to use. Additionally, searches and downloaded the information of the chip using the GPL ID to obtain the .soft file.
 #' @param GSE The list of GSE ID.
 #' @param GPL The GPL ID of the chip used in the microarray experiment.
-#' @param dir The pathfile to download and uncompress the raw expression data, by default is the current pathfile.
+#' @param dir The path file to download and uncompress the raw expression data, by default is the current path file.
 #' @return A set of files with the uncompressed data and ready to use.
 #' @seealso \code{\link{get.affy}} to charge the expression data.
 #' @examples 
 #' 
-#' # Extract data from GEO DataSet
+#' # Extracts data from GEO DataSets
 #' 
 #' get.info(GSE = "GSE8216", GPL = "GPL2025",dir = tempdir())
 
