1ba53f8856efa2ca3811314de828172898c0751b
Juan Henao <judhenaosa@unal.edu.co>
2017-03-05 21:35:07 -0500

    Update coexnet
    Former-commit-id: 92165ef7065eddab6b42beaa79074cc6a36b0d61
    Former-commit-id: 53f7efa39f4af7f3707f286ffd596f56b9afd8d9

--- R/CCP.R
+++ R/CCP.R
@@ -8,6 +8,9 @@
 #' by deleting the solitary nodes in the intersection network. They must be graph objects.
 #' @param ... The networks (igraph objects) to obtain the Common Connection Patterns.
 #' @return An igraph object with all the Common Connection Pattern in the same network.
+#' @details 
+#' The Common Connection Pattern (CCP), is a new methodological proposal to identify molecular components linked together and common in several biological networks. The principal assumption behind Common Connection Pattern is that the networks to be compared must have the same molecular information from, i.e., must compare one layer of molecular abstraction at the same time, for example, co-expression layer, protein-protein layer, the gene regulation layer, among others.
+#' In general, the comparison of biological networks is made to determine common elements or biomarkers among several related phenotypes. In this case, the Common Connection Patterns aims to identify common molecular elements between these phenotypes that are associated also with each other in a specific way. For this, the intersection between biological networks is calculated whose result can have two kinds of elements. On the one hand, the shared nodes without any connection with other nodes in the intersection network. On the other hand, nodes connected to one or more nodes in the intersection network. Each connected component in the intersection will be considered as a Common Connection Pattern.
 #' @examples 
 #' # Loading data
 #' 
